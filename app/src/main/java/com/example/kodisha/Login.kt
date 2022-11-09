a5d9f12aefa302627ead927e3a77241dd8344ae7
Hamez-brayo <kariukibrian090@gmail.com>
2022-11-09 16:12:02 +0300

    addition of the navigation graph

--- app/src/main/java/com/example/kodisha/Login.kt
+++ app/src/main/java/com/example/kodisha/Login.kt
@@ -5,13 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 
 class Login : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,5 +16,12 @@ class Login : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        view.findViewById<Button>(R.id.LoginBTN).setOnClickListener{
+            //view.findNavController().navigate(R.id.action_loginfragment_to_registerfragment)
+
+        }
     }
 }
